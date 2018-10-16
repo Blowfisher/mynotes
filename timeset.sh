4cc3288d15624cb609bbba9da8b529568bc92bdb
blowfisher <2305901977@qq.com>
2018-10-16 10:32:08 +0800

    集群时间设定

---
+++ timeset.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#设定时区
+#ansible kube  -m command -a "ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime"
+
+
+
+#设定时间
+cmd="date --date=@`date +%s`"
+data=`${cmd}`
+ansible kube -m command -a "date -s '$data'"
