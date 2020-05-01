50c9161ec6aeb717f514da028223f9b9ec4db44f
Blowfisher <33476715+Blowfisher@users.noreply.github.com>
2020-05-01 13:17:37 +0800

    Update timeset.sh

--- timeset.sh
+++ timeset.sh
@@ -3,7 +3,8 @@
 #设定时区
 #ansible kube  -m command -a "ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime"
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-
+yum -y install ntpdate
+ntpdate ntp1.aliyun.com
 
 #设定时间
 cmd="date --date=@`date +%s`"
